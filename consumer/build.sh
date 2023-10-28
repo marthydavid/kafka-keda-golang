20304ac8e98935b2a945927ccc97f81279ae590b
marthydavid <marthy.david@gmail.com>
2023-10-29 01:18:39 +0200

    Switch to goreleaser

--- consumer/build.sh
+++
@@ -1,4 +0,0 @@
-if [ ${GOARCH} == amd64 ]
-CC=aarch64-linux-gnu-gcc
-fi
-CGO_ENABLED=1 GOOS=${TARGETOS} GOARCH=${TARGETARCH} go build -tags musl --ldflags "-extldflags -static" -o consumer
--- consumer/go.mod
+++ consumer/go.mod
@@ -1,6 +1,6 @@
 module github.com/marthydavid/kafka-keda-golang/consumer
 
-go 1.21.3
+go 1.21
 
 require (
 	github.com/confluentinc/confluent-kafka-go/v2 v2.3.0
